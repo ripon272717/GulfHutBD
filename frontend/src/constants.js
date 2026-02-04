ca9e55b5858692d4c1aa6dc6d19c515fbcaabce1
ripon272717 <mojam688388@gmail.com>
2026-02-04 13:31:01 +0300

    Fixing server production build

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,7 @@
-// এই কোডটি লোকালহোস্ট এবং লাইভ সার্ভার দুইটাই অটোমেটিক হ্যান্ডেল করবে
-export const BASE_URL = 
-  process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:5000' 
-    : ''; // লাইভ সার্ভারে এটা খালি থাকবে কারণ ফ্রন্টএন্ড-ব্যাকএন্ড একই ডোমেইনে থাকে
+// এটি লোকাল এবং সার্ভার দুইটাই অটোমেটিক হ্যান্ডেল করবে
+export const BASE_URL = process.env.NODE_ENV === 'development' 
+  ? 'http://localhost:5000' 
+  : ''; 
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
